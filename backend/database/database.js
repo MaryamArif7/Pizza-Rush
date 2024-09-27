61a268de984ee1e32cafeb202a7b4980f1fe8d52
MaryamArif7 <www.maryamarif28242@gmail.com>
2024-09-27 12:49:19 +0500

    db connected

---
+++ backend/database/database.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+const connectDB =async()=>{
+    try{
+        mongoose.connect(process.env.DB_URI);
+        console.log("MongoDB connected sucessfully")
+    }
+    catch{
+        console.error("Failed to connect with Mongo")
+    }
+}
+export default connectDB();
--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,21 @@
 import express from 'express';
 import dotenv from "dotenv";
-import cors from 'cors'
-dotenv.config();
-//cors.config()
-
+import cookieParser from "cookie-parser"
+import cors from 'cors';
+import connectDB from "./database/database.js";
+dotenv.config({});
+app.use(express.json());
+app.use(express.urlencoded());
+const corsOpitons={
+    origin:'http://localhost:5173',
+    credentials:true
+}
+app.use(cors(corsOpitons))
+app.use(cookieParser())
 const PORT=5000 || process.env.PORT;
 
 const app=express();
 app.listen(PORT,()=>{
-  
+     connectDB();
     console.log(`Server is runing on PORT ${PORT}`);
 })
